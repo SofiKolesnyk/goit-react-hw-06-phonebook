d5f3b9032a7f7d8f32f7664d1a8891340254f01b
SofiKolesnyk <sofi_serg@ukr.net>
2023-02-05 17:59:59 +0200

    hw

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+  display: block;
+  padding: 10px 10px;
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 20px;
+  background-color: #f799cb;
+  border: 2px solid black;
+  width: 300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 40px;
+  padding-left: 40px;
+`;
+
+export const Input = styled.input`
+  display: block;
+  width: 290px;
+  margin-top: 5px;
+  outline: none;
+`;
---
+++ src/components/Title/Ttitle.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const TextTitle = styled.h2`
+  padding: 10px 10px;
+  background-color: grey;
+  width: 300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 40px;
+  padding-left: 40px;
+`;
