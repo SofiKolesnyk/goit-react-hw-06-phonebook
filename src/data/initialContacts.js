f6e855a35fbc66268650211e5ae1f498d38e4921
SofiKolesnyk <sofi_serg@ukr.net>
2023-02-07 20:14:46 +0200

    Update initialContacts.js

--- src/data/initialContacts.js
+++ src/data/initialContacts.js
@@ -1,6 +1,7 @@
 export const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-1', name: 'Anna Krot', number: '546-345-343' },
+  { id: 'id-2', name: 'Hellen Adonn', number: '380-123-765' },
+  { id: 'id-3', name: 'Eldar Mixienko', number: '665-456-987' },
+  { id: 'id-4', name: 'Sofi Kolesnyk', number: '-234-543-545' },
+  { id: 'id-4', name: 'Eugene Kolesnyk', number: '227-766-487' },
 ];
