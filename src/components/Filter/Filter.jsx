206d1e1eae94e7c60421870c353f69e4217ed462
SofiKolesnyk <sofi_serg@ukr.net>
2023-02-07 20:07:20 +0200

    hw

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,22 @@
-import { Label, Input } from './Filter.styled';
-import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContactFilter } from 'redux/selectors';
+import { setContactFilter } from 'redux/filterSlice';
+import { Wrapper, Label, Input } from './Filter.styled';
 
-export const Filter = ({ filterContact }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getContactFilter);
+
   return (
-    <div>
-      <Label>
-        Find contacts by name
-        <Input type="text" onChange={filterContact}></Input>
-      </Label>
-    </div>
+    <Wrapper>
+      <Label htmlFor="filter">Find contacts by name</Label>
+      <Input
+        name="filter"
+        type="text"
+        id="filter"
+        value={filter}
+        onChange={e => dispatch(setContactFilter(e.currentTarget.value))}
+      />
+    </Wrapper>
   );
-};
-
-Filter.propType = {
-  filterContact: PropTypes.func.isRequired,
 };
